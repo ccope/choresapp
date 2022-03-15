ecc28d621956c1b8dd942cd9b2b2362540a51536
Cam Cope <github@camcope.me>
2022-03-15 16:08:43 -0700

    add email provider abstraction, gmail implementation

--- chores/chores.py
+++ chores/chores.py
@@ -4,9 +4,11 @@ import json
 import os
 from datetime import datetime
 from email.message import Message
+from typing import Optional
 
-import boto.ses
 from flask import Flask, render_template, request
+
+from chores.email import EmailProvider
 
 with open('data/chores.json') as choresjson:
     status = json.load(choresjson)
@@ -14,6 +16,15 @@ with open('data/chores.json') as choresjson:
 template_dir = os.path.abspath('templates')
 app = Flask(__name__, template_folder=template_dir)
 fmt = '%a, %d %b %Y %H:%M:%S %z'
+
+def get_email_provider(name: Optional[str]) -> EmailProvider:
+    if name == "AWS":
+        from chores.email.aws import SesEmail
+        return SesEmail()
+    elif name == "Google":
+        from chores.email.gmail import GMailEmail
+        return GMailEmail()
+    raise Exception("Invalid email provider!")
 
 @app.route('/')
 def displaychores():
@@ -47,9 +58,7 @@ def nag():
     msg['Cc'] = "" # TODO change before release
     msg['Reply-To'] = ""
     msg.preamble = "\n"
-    conn = boto.ses.connect_to_region('us-east-1')
-    conn.send_raw_email(msg.as_string())
-    conn.close()
+    g.email.send(msg)
     return """<!doctype html>
 <html><head></head><body>%s has been nagged to %s.<script>setTimeout(function() { window.location.assign("%s"); }, 2500);</script></body></html>""" % (doer, chore, request.url_root)
 
@@ -74,11 +83,15 @@ def done():
     msg['Cc'] = "" # TODO change before release
     msg['Reply-To'] = ""
     msg.preamble = "\n"
-    conn = boto.ses.connect_to_region('us-east-1')
-    conn.send_raw_email(msg.as_string())
-    conn.close()
+    try:
+        g.email.send(msg)
+    except Exception as e:
+        print("failed to email: %s" % e)
     return """<!doctype html>
 <html><head></head><body>Thanks.<script>setTimeout(function() { window.location.assign("%s"); }, 2500);</script></body></html>""" % request.url_root
 
 if __name__ == '__main__':
+    email_provider_name = os.environ.get("EMAIL_PROVIDER")
+    with get_email_provider(email_provider_name) as email_provider:
+        g.email = email_provider
     app.run(debug=False, host='0.0.0.0', port=9001)
