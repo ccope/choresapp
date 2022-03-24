0dc568cfe59ff23ae3997ed7dd06b9f9e3d67b13
Cam Cope <github@camcope.me>
2022-03-23 18:29:26 -0700

    move more things to env vars

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy_session import flask_scoped_session
@@ -10,12 +11,12 @@ from chores.email_service import get_email_provider
 from chores.web import app
 
 
-DBURI = "sqlite+pysqlite:///chores.db"
-engine = create_engine(DBURI, connect_args={"check_same_thread": False},
+load_dotenv()
+engine = create_engine(os.environ['DBURI'], connect_args={"check_same_thread": False},
                        echo=True, future=True)
 SessionFactory = sessionmaker(autoflush=False, bind=engine)
 flask_scoped_session(SessionFactory, app)
-email_provider_name = os.environ.get("EMAIL_PROVIDER")
+email_provider_name = os.environ.get("NOTIFICATIONS")
 email_provider = get_email_provider(email_provider_name)
 app.config['email'] = email_provider
 app.run(debug=False, host='0.0.0.0', port=9001)
