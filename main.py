6b397b23c461fc22174cd6f3d32f8245fbc9d399
Cam Cope <github@camcope.me>
2022-03-25 18:42:12 -0700

    cleanup

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy_session import flask_scoped_session
 
 from chores.notification_service import get_notification_provider
-#from chores.models.choresdb import Assignments, People, Tasks, Timers
 from chores.web import app
 
 
 load_dotenv()
-engine = create_engine(os.environ['DBURI'], connect_args={"check_same_thread": False},
-                       echo=True, future=True)
+engine = create_engine(
+        os.environ['DBURI'],
+        connect_args={"check_same_thread": False},
+        future=True
+        )
 SessionFactory = sessionmaker(autoflush=False, bind=engine)
 flask_scoped_session(SessionFactory, app)
 notification_provider_name = os.environ.get("NOTIFICATIONS")
