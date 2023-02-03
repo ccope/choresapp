8f92c9f13dd34a53ff3c86614d2d074ffaec3850
Cam Cope <github@camcope.me>
2023-02-02 16:42:14 -0800

    update db model to fix type annotations

--- chores/models/choresdb.py
+++ chores/models/choresdb.py
@@ -1,42 +1,34 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, Text
-from sqlalchemy.orm import Mapped, declarative_base, relationship
+from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column, relationship
 from typing import Any, List, TYPE_CHECKING
 
-# TODO: remove when sqlalchemy 2.0 types work properly
-if TYPE_CHECKING:
-    from sqlalchemy.orm.decl_api import DeclarativeMeta
-
-    class Base(metaclass=DeclarativeMeta):
-        __abstract__ = True
-
-else:
-    Base = declarative_base()
-
+class Base(DeclarativeBase):
+    pass
 
 class People(Base):
     __tablename__ = "people"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False, unique=True)
-    email = Column(String(128), nullable=False, unique=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(128), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(128), nullable=False, unique=True)
     tasks: Mapped[List["Assignments"]] = relationship("Assignments")
 
 
 class Tasks(Base):
     __tablename__ = "tasks"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    description = Column(Text)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(128), nullable=False)
+    description: Mapped[str] = mapped_column(Text)
     people: Mapped[List["Assignments"]] = relationship("Assignments")
 
 
 class Assignments(Base):
     __tablename__ = "assignments"
 
-    task_id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
-    people_id = Column(Integer, ForeignKey("people.id"), primary_key=True)
-    counter = Column(Integer, default=0)
+    task_id: Mapped[int] = mapped_column(Integer, ForeignKey("tasks.id"), primary_key=True)
+    people_id: Mapped[int] = mapped_column(Integer, ForeignKey("people.id"), primary_key=True)
+    counter: Mapped[int] = mapped_column(Integer, default=0)
     task: Mapped["Tasks"] = relationship("Tasks", back_populates="people")
     person: Mapped["People"] = relationship("People", back_populates="tasks")
 
@@ -44,6 +36,6 @@ class Assignments(Base):
 class Timers(Base):
     __tablename__ = "timers"
 
-    id = Column(Integer, primary_key=True)
-    task_id = Column(Integer, ForeignKey("tasks.id"), nullable=False)
-    frequency = Column(Integer, default=72)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    task_id: Mapped[int] = mapped_column(Integer, ForeignKey("tasks.id"), nullable=False)
+    frequency: Mapped[int] = mapped_column(Integer, default=72)
