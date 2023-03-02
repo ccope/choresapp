7bc31805cfbd8997a6829a8bf9a83d71ce5858a3
Cam Cope <github@camcope.me>
2023-03-01 22:41:39 -0800

    new assignment leaderboard function for autonag

--- chores/lib.py
+++ chores/lib.py
@@ -6,27 +6,29 @@ from sqlalchemy.orm import selectinload
 from chores.models.choresdb import Assignments, People, Tasks
 
 
-def get_assignees_sorted_by_count(session, task_obj: Tasks) -> List[People]:
-    assigned_people = (
-        session.execute(
+def get_assignment_leaderboard(session, task_obj: Tasks) -> List[Assignments]:
+    return session.execute(
             select(Assignments)
             .options(selectinload(Assignments.person))
             .where(Assignments.task_id == task_obj.id)
             .order_by(Assignments.counter.asc())
-        )
-        .scalars()
-        .all()
-    )
+            ).scalars().all()
+
+def get_assignees_sorted_by_count(session, task_obj: Tasks) -> List[People]:
+    assignments = get_assignment_leaderboard(session, task_obj)
+    if len(assignments) == 0:
+        return []
     # If this is a fresh task, ensure the order is random
     # all tasks may be fresh so the same person could get pinged every time
-    if assigned_people[0].counter == 0:
-        zeros = []
-        for a in assigned_people:
+    if assignments[0].counter == 0:
+        zeros: List[Assignments] = []
+        for a in assignments:
             if a.counter == 0:
                 zeros.append(a)
-        rand = random.choice(zeros)
-        person_obj = rand.person
-    else:
-        person_obj = assigned_people[0].person
-    the_rest = [p.person for p in assigned_people if not p.people_id == person_obj.id]
-    return [person_obj] + the_rest
+            else:
+                break
+        if len(zeros) > 1:
+            random.shuffle(zeros)
+            assignments = zeros + assignments[len(zeros):]
+
+    return [p.person for p in assignments]
