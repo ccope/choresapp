6abfa524a9292433f6137a20bb5716800b6cb927
Cam Cope <github@camcope.me>
2026-01-10 22:04:19 -0800

    remove old migrations, start fresh in default location

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ notification_provider = get_notification_provider(notification_provider_name)
 app.config["notifyer"] = notification_provider
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DBURI"]
 db.init_app(app)
-Migrate(app, db, directory="choresdb")
+Migrate(app, db, render_as_batch=True)
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=9001)
